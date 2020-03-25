8c1b9e8203814d1b1c43f2f44d79aa936a284696
Calvin <cod11@georgetown.edu>
2020-03-24 20:15:07 -0400

    added __name__ == __main__ fix

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -47,172 +47,173 @@ def recommendationEngine(averagePrice, latestClose):
 
 def hasSpecial(inputString):
     return not inputString.isalnum()
-    
-#Ask user to submit choice, conver to upper case
-user_symbols = input(
-    "Please enter a ticker symbol, or a series of ticker symbols seperated by a comma, to retrieve data: "
-).upper()
 
-#Split up symbols and remove trailing white space
-user_symbols = list(map(stripString, user_symbols.split(',')))
-#Perfom error checking on each submission
-for symbol in user_symbols:
-    #Check that ticker is less than 5 characters
-    if (len(symbol) > 4):
-        print("[ERROR]: Symbol '" + str(symbol) +
-              "' has too many characters. Please try again.")
-        failure = True
-    #Check that ticker contains only contains letters
-    if (hasNumbers(symbol)):
-        print("[ERROR]: Symbol '" + str(symbol) +
-              "' contains numbers. Please try again.")
-        failure = True
-    #Check that the ticker contains special characters
-    if(hasSpecial(symbol)):
-        print("[ERROR]: Symbol '" + str(symbol) +
-              "' contains a special character. Please try again.")
-        failure = True
+if __name__ == "__main__":   
+    #Ask user to submit choice, conver to upper case
+    user_symbols = input(
+        "Please enter a ticker symbol, or a series of ticker symbols seperated by a comma, to retrieve data: "
+    ).upper()
 
-#If there are errors in the first wave, make requests for information
+    #Split up symbols and remove trailing white space
+    user_symbols = list(map(stripString, user_symbols.split(',')))
+    #Perfom error checking on each submission
+    for symbol in user_symbols:
+        #Check that ticker is less than 5 characters
+        if (len(symbol) > 4):
+            print("[ERROR]: Symbol '" + str(symbol) +
+                "' has too many characters. Please try again.")
+            failure = True
+        #Check that ticker contains only contains letters
+        if (hasNumbers(symbol)):
+            print("[ERROR]: Symbol '" + str(symbol) +
+                "' contains numbers. Please try again.")
+            failure = True
+        #Check that the ticker contains special characters
+        if(hasSpecial(symbol)):
+            print("[ERROR]: Symbol '" + str(symbol) +
+                "' contains a special character. Please try again.")
+            failure = True
 
-if (not failure):
-    #Begin output
-    print("-------------------------")
-    print("SELECTED SYMBOLS: ", ", ".join(user_symbols))
-    print("-------------------------")
-    print("REQUESTING STOCK MARKET DATA...")
-    print("REQUEST AT: ", datetime.datetime.now().strftime("%D %I:%M%p"))
-    print("-------------------------")
+    #If there are errors in the first wave, make requests for information
 
-    #Making requests
-    for ticker in user_symbols:
-        #creating the payload to send with the request
-        payload = {
-            'function': 'TIME_SERIES_DAILY',
-            'symbol': ticker,
-            'datatype': 'json',
-            'apikey': API_KEY
-        }
+    if (not failure):
+        #Begin output
+        print("-------------------------")
+        print("SELECTED SYMBOLS: ", ", ".join(user_symbols))
+        print("-------------------------")
+        print("REQUESTING STOCK MARKET DATA...")
+        print("REQUEST AT: ", datetime.datetime.now().strftime("%D %I:%M%p"))
+        print("-------------------------")
 
-        try:
-            request_result = requests.get("https://www.alphavantage.co/query",
-                                          params=payload).json()
-
-            if ("Error Message" in request_result):
-                raise Exception(request_result["Error Message"])
-
-            results[ticker] = request_result  #adding request info to an array
-
-            path = os.path.join(os.path.dirname(__file__),"..", "data", ticker + "_data.csv")
-            csvFile = open(path,
-                           'w')  #open up csv to write to
-            writer = csv.writer(csvFile)  #create csv writer
-            writer.writerow(["time", "open", "high", "low", "close",
-                             "volume"])  #insert header row
-
-            #Init variables for ouptut statistics for the ticker
-            latest_day = ""
-            last_refreshed = request_result["Meta Data"]["3. Last Refreshed"]
-            latest_close = 0.00
-            recent_high = 0.00
-            recent_low = 0.00
-            recommendation = ""
-            reason = ""
-            averagePrice = 0.00
-            sumPrice = 0.00
-
-            firstTick = 0  #using this to capture only the first result
-
-            for time in request_result[
-                    "Time Series (Daily)"]:  #add all the info
-                time_series_values = list(
-                    request_result["Time Series (Daily)"][time].values())
-
-                if (firstTick == 0):
-                    date_values = time.split("-")
-                    latest_day = datetime.date(year=int(date_values[0]),
-                                               month=int(date_values[1]),
-                                               day=int(date_values[2]))
-                    latest_close = float(time_series_values[3])
-                    recent_low = float(time_series_values[2])
-                    firstTick = firstTick + 1
-
-                writer.writerow([time] + time_series_values)
-
-                if (recent_high < float(time_series_values[1])):
-                    recent_high = float(time_series_values[1])
-
-                if (recent_low > float(time_series_values[2])):
-                    recent_low = float(time_series_values[2])
-                
-                #summing all of the closing prices together for the average later on
-                sumPrice = sumPrice + float(time_series_values[3])
-
-            #sum of all prices divided by the number of points to get average over the period
-            averagePrice = sumPrice / len(request_result["Time Series (Daily)"])
-
-            [recommendation, reason] = recommendationEngine(averagePrice, latest_close)
-
-            #Output the data
-            results[ticker]["Summary Data"] = {
-                "last_refreshed": last_refreshed,
-                "latest_day": latest_day,
-                "latest_close": to_usd(latest_close),
-                "recent_high": to_usd(recent_high),
-                "recent_low": to_usd(recent_low),
-                "ticker": ticker,
-                "recommendation": recommendation,
-                "reason": reason,
-                "averagePrice": to_usd(averagePrice)
+        #Making requests
+        for ticker in user_symbols:
+            #creating the payload to send with the request
+            payload = {
+                'function': 'TIME_SERIES_DAILY',
+                'symbol': ticker,
+                'datatype': 'json',
+                'apikey': API_KEY
             }
 
-            print("[SUCCESS] " + ticker + " data file has been created")
+            try:
+                request_result = requests.get("https://www.alphavantage.co/query",
+                                            params=payload).json()
 
-        except:
-            print(
-                "[ERROR]: An error with the request occured. Skipping ticker "
-                + ticker + " | Response:  " + request_result["Error Message"])
+                if ("Error Message" in request_result):
+                    raise Exception(request_result["Error Message"])
 
-    tickers = results.keys()
+                results[ticker] = request_result  #adding request info to an array
 
-    tableData = {
-        "ticker": [],
-        "last_refreshed": [],
-        "latest_day": [],
-        "latest_close": [],
-        "recent_high": [],
-        "recent_low": []
-    }
+                path = os.path.join(os.path.dirname(__file__),"..", "data", ticker + "_data.csv")
+                csvFile = open(path,
+                            'w')  #open up csv to write to
+                writer = csv.writer(csvFile)  #create csv writer
+                writer.writerow(["time", "open", "high", "low", "close",
+                                "volume"])  #insert header row
 
-    recommendationData = {
-        "ticker": [],
-        "recommendation": [],
-        "reason": [],
-        "averagePrice": []
-    }
+                #Init variables for ouptut statistics for the ticker
+                latest_day = ""
+                last_refreshed = request_result["Meta Data"]["3. Last Refreshed"]
+                latest_close = 0.00
+                recent_high = 0.00
+                recent_low = 0.00
+                recommendation = ""
+                reason = ""
+                averagePrice = 0.00
+                sumPrice = 0.00
 
-    for symbol in tickers:
-        for key in tableData.keys():
-            tableData[key].append(results[symbol]["Summary Data"][key])
+                firstTick = 0  #using this to capture only the first result
+
+                for time in request_result[
+                        "Time Series (Daily)"]:  #add all the info
+                    time_series_values = list(
+                        request_result["Time Series (Daily)"][time].values())
+
+                    if (firstTick == 0):
+                        date_values = time.split("-")
+                        latest_day = datetime.date(year=int(date_values[0]),
+                                                month=int(date_values[1]),
+                                                day=int(date_values[2]))
+                        latest_close = float(time_series_values[3])
+                        recent_low = float(time_series_values[2])
+                        firstTick = firstTick + 1
+
+                    writer.writerow([time] + time_series_values)
+
+                    if (recent_high < float(time_series_values[1])):
+                        recent_high = float(time_series_values[1])
+
+                    if (recent_low > float(time_series_values[2])):
+                        recent_low = float(time_series_values[2])
+                    
+                    #summing all of the closing prices together for the average later on
+                    sumPrice = sumPrice + float(time_series_values[3])
+
+                #sum of all prices divided by the number of points to get average over the period
+                averagePrice = sumPrice / len(request_result["Time Series (Daily)"])
+
+                [recommendation, reason] = recommendationEngine(averagePrice, latest_close)
+
+                #Output the data
+                results[ticker]["Summary Data"] = {
+                    "last_refreshed": last_refreshed,
+                    "latest_day": latest_day,
+                    "latest_close": to_usd(latest_close),
+                    "recent_high": to_usd(recent_high),
+                    "recent_low": to_usd(recent_low),
+                    "ticker": ticker,
+                    "recommendation": recommendation,
+                    "reason": reason,
+                    "averagePrice": to_usd(averagePrice)
+                }
+
+                print("[SUCCESS] " + ticker + " data file has been created")
+
+            except:
+                print(
+                    "[ERROR]: An error with the request occured. Skipping ticker "
+                    + ticker + " | Response:  " + request_result["Error Message"])
+
+        tickers = results.keys()
+
+        tableData = {
+            "ticker": [],
+            "last_refreshed": [],
+            "latest_day": [],
+            "latest_close": [],
+            "recent_high": [],
+            "recent_low": []
+        }
+
+        recommendationData = {
+            "ticker": [],
+            "recommendation": [],
+            "reason": [],
+            "averagePrice": []
+        }
+
+        for symbol in tickers:
+            for key in tableData.keys():
+                tableData[key].append(results[symbol]["Summary Data"][key])
+            
+            for key in recommendationData.keys():
+                recommendationData[key].append(results[symbol]["Summary Data"][key])
+
+        print(
+            " \n \n",
+            tabulate(tableData,
+                    headers=[
+                        "Ticker", "Last Refreshed", "Latest Day", "Latest Close",
+                        "Recent High", "Recent Low"
+                    ],
+                    tablefmt="grid"))
         
-        for key in recommendationData.keys():
-            recommendationData[key].append(results[symbol]["Summary Data"][key])
+        print(
+            " \n \n",
+            tabulate(recommendationData,
+                    headers=[
+                        "Ticker", "Recommendation", "Reason", "Average Price"
+                    ],
+                    tablefmt="grid"))
 
-    print(
-        " \n \n",
-        tabulate(tableData,
-                 headers=[
-                     "Ticker", "Last Refreshed", "Latest Day", "Latest Close",
-                     "Recent High", "Recent Low"
-                 ],
-                 tablefmt="grid"))
-    
-    print(
-        " \n \n",
-        tabulate(recommendationData,
-                 headers=[
-                     "Ticker", "Recommendation", "Reason", "Average Price"
-                 ],
-                 tablefmt="grid"))
-
-    print("End of trading recommendations report. Good bye!")
+        print("End of trading recommendations report. Good bye!")
