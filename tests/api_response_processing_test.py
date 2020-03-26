bf7e04dfdbc0ed5b3e2aa5bf86afe51d9e30d8de
Calvin <cod11@georgetown.edu>
2020-03-26 14:11:33 -0400

    refactored tests

--- tests/api_response_processing_test.py
+++ tests/api_response_processing_test.py
@@ -4,16 +4,49 @@ from app.robo_advisor import compile_url
 import os
 import pytest
 
-CI_ENV = os.environ.get("CI") == "true"
+test_response = {
+    "Meta Data": {
+        "1. Information": "Daily Prices (open, high, low, close) and Volumes",
+        "2. Symbol": "MSFT",
+        "3. Last Refreshed": "2018-06-08",
+        "4. Output Size": "Full size",
+        "5. Time Zone": "US/Eastern"
+    },
+    "Time Series (Daily)": {
+        "2019-06-08": {
+            "1. open": "101.0924",
+            "2. high": "101.9500",
+            "3. low": "100.5400",
+            "4. close": "101.6300",
+            "5. volume": "22165128"
+        },
+        "2019-06-07": {
+            "1. open": "102.6500",
+            "2. high": "102.6900",
+            "3. low": "100.3800",
+            "4. close": "100.8800",
+            "5. volume": "28232197"
+        },
+        "2019-06-06": {
+            "1. open": "102.4800",
+            "2. high": "102.6000",
+            "3. low": "101.9000",
+            "4. close": "102.4900",
+            "5. volume": "21122917"
+        }
+    }
+}
 
-@pytest.mark.skipif(CI_ENV==True, reason="to avoid issuing HTTP requests on the CI server") # skips this test on CI
-def test_process_data(stock_data):  
+def test_process_data():  
     try:
-        data = StockData(stock_data)
+        data = StockData(test_response)
         result = data.get_stats()
 
         assert type(result["latest_close"]) == float
         assert isinstance(result, dict)
+        assert result["average_price"] == 101.66666666666667
+        assert result["recent_high"] == 102.69
+        assert result["recent_low"] == 101.9
     
     except:
         assert False
--- tests/make_request_test.py
+++ tests/make_request_test.py
@@ -8,9 +8,9 @@ CI_ENV = os.environ.get("CI") == "true"
 @pytest.mark.skipif(CI_ENV==True, reason="to avoid issuing HTTP requests on the CI server") # skips this test on CI
 def test_get_response_metadata(stock_data):
     result = stock_data
-    assert result.get('Meta Data', False)
+    assert 'Meta Data' in result.keys()
 
 @pytest.mark.skipif(CI_ENV==True, reason="to avoid issuing HTTP requests on the CI server") # skips this test on CI
 def test_get_response_timeseries(stock_data):
     result = stock_data
-    assert result.get("Time Series (Daily)", False)
+    assert "Time Series (Daily)" in result.keys()
